3b7b10b50f66481e3e5331a552db9f4ff542511e
eovywjr1 <eovywjr1@naver.com>
2024-07-09 01:32:59 +0900

    Add files via upload

---
+++ UnrealInterface/MyGameInstance.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyGameInstance.h"
+#include "Person.h"
+#include "Student.h"
+#include "Teacher.h"
+#include "Staff.h"
+
+void UMyGameInstance::Init()
+{
+	Super::Init();
+
+	TArray<UPerson*> Persons = { NewObject<UStudent>(), NewObject<UTeacher>(), NewObject<UStaff>() };
+
+	for (const UPerson* Person : Persons)
+	{
+		UE_LOG(LogTemp, Log, TEXT("구성원 이름 : %s"), *Person->GetName());
+	}
+
+	for (const UPerson* Person : Persons)
+	{
+		const ILessonInterface* LessonInterface = Cast<ILessonInterface>(Person);
+		if (LessonInterface)
+		{
+			UE_LOG(LogTemp, Log, TEXT("%s님은 수업에 참여할 수 있습니다."), *Person->GetName());
+			LessonInterface->DoLesson();
+		}
+		else 
+		{
+			UE_LOG(LogTemp, Log, TEXT("%s님은 수업에 참여할 수 없습니다."), *Person->GetName());
+		}
+	}
+}
---
+++ UnrealInterface/Student.cpp
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Student.h"
+
+UStudent::UStudent()
+{
+	Name = TEXT("이학생");
+}
+
+void UStudent::DoLesson() const
+{
+	ILessonInterface::DoLesson();
+	UE_LOG(LogTemp, Log, TEXT("%s님이 공부합니다."), *Name);
+}
