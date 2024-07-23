0cc22c2bf7a4f6ed1b9756fbe64ead83634e0a08
eovywjr1 <eovywjr1@naver.com>
2024-07-24 03:42:59 +0900

    Add files via upload

---
+++ UnrealBuildSystem/UnrealBuildSystem/UnrealBuildSystemGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealBuildSystemGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALBUILDSYSTEM_API AUnrealBuildSystemGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
